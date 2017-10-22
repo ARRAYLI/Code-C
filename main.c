21c1fbceab4578236a1b0f01c730fdbd85da036c
ARRAYLI <32920845+ARRAYLI@users.noreply.github.com>
2017-10-22 13:08:08 +0800

    正方形

--- main.c
+++ main.c
@@ -1,29 +1,13 @@
 #include<stdio.h>
 int main(void)
 {
-	int num1, num2, num3,temp;
-	printf("请输入三个整数：\n");
-	scanf("%d%d%d",&num1,&num2,&num3);
-	if (num1 >num2)
-	{
-		temp = num1;
-		num1 = num2;
-		num2 = temp;
-	}
-	if (num1 > num3)
-	{
-		temp = num1;
-		num1 = num3;
-		num3 = temp;
-	}
-	if (num2 > num3)
-	{
-		temp = num2;
-		num2 = num3;
-		num3 = temp;
-	}
-	printf("排完序后的顺序为：%4d %4d %4d\n",num1,num2,num3);
 
+	printf("* * * * *\n");
+	printf("*       *\n");
+	printf("*       *\n");
+	printf("*       *\n");
+
+	printf("* * * * *\n");
 
 	return 0;
 }
